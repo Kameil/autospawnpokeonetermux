6992df80e32adc23631a4d46cfea4b5992ac79fa
Kameil <130675534+Kameil@users.noreply.github.com>
2024-01-10 20:28:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,14 @@ def download_file(url, file_name):
         return True
     return False
 
-github_url = "https://raw.githubusercontent.com/Kameil/arquivos-para-eu-da-uns-request-tendeu/main/pokeones.py"
+github_url = "https://raw.githubusercontent.com/Kameil/autospawnpokeonetermux/main/bot.py"
 local_file_name = "bot.py" 
 
-verificar1 = os.environ.get('user_token', '')
-verificar2 = os.environ.get('chat_id', '')
-verificar3 = os.environ.get('prefix', '')
+
 
 if download_file(github_url, local_file_name):
     try:
-        if verificar1 and verificar2 and verificar3:
-            subprocess.run(["python3", local_file_name], check=True)
-        else:
-            print(colored('ops, parece que voce nao tem todos os secretes', 'red'), colored('verifique se voce possui os secrets:', 'blue'), colored('\nuser_token\nchat_id\nprefix', 'yellow'))
+        subprocess.run(["python3", local_file_name], check=True)
     except subprocess.CalledProcessError:
         print(colored('Erro ao executar o arquivo.', "red"), colored(' Entre em contato com o discord', 'yellow'))
       
